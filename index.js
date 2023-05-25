c3d3380fed5b3424ba1367cab623417954130410
Cheyenne Edwards <112296933+CheyenneTech@users.noreply.github.com>
2023-05-24 21:50:21 -0400

    working on pop up modals

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// creating a pop up box that will blur the rest of the website when the box appears. You will be able to both click the X button, and also click out anywhere in the webpage.
+
 function toggle(){
     let blur = document.getElementById('blur');
     blur.classList.toggle('active');
+    let skillPopUp = document.getElementById('skills-pop-up-box');
+    skillPopUp.classList.toggle('active');
+    let experiencePopUp = document.getElementById('experience-pop-up-box');
+    experiencePopUp.classList.toggle('active');
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 
+This project is a digital resume meant to display my accomplishments. I am using a combination of html, css, and vanilla javascript. 
+
+I am using a grid layout, with muted &warmer toned color pallet. I used grid so I could utilize the grid gap property.
