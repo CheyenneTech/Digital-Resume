9b5bbe619163bd2fe04f4cc8e0ddf492ba10d95c
Cheyenne Edwards <112296933+CheyenneTech@users.noreply.github.com>
2023-05-25 23:23:29 -0400

    pop up boxes/projects page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// creating a pop up box that will blur the rest of the website when the box appears. You will be able to both click the X button, and also click out anywhere in the webpage.
+// creating a pop up box that will blur the rest of the website when the box appears. 
 
-function toggle(){
-    let blur = document.getElementById('blur');
+
+function toggle(id){
+    const blur = document.getElementById('blur');
     blur.classList.toggle('active');
-    let skillPopUp = document.getElementById('skills-pop-up-box');
-    skillPopUp.classList.toggle('active');
-    let experiencePopUp = document.getElementById('experience-pop-up-box');
-    experiencePopUp.classList.toggle('active');
+
+    const currentPopUp = document.getElementById(id);
+    currentPopUp.classList.toggle('active');
+
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.toggle('active');
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 This project is a digital resume meant to display my accomplishments. I am using a combination of html, css, and vanilla javascript. 
 
 I am using a grid layout, with muted &warmer toned color pallet. I used grid so I could utilize the grid gap property.
+
+
+    
